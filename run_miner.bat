1d40072440f96e383e3ce95ae9ed48f9e01a981b
hovancuong11 <162858124+hovancuong11@users.noreply.github.com>
2024-07-06 06:18:02 +0700

    Add files via upload

---
+++ run_miner.bat
@@ -0,0 +1,6 @@
+REM For more usage info: hellminer.exe --help
+
+REM Example SSL URL
+REM .\hellminer.exe -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+.\hellminer -c stratum+tcp://na.luckpool.net:3960 -u RRkCDQXQv5utVb9AipH2AUWouxFQfTfddJ.Rig-1 -p hybrid --cpu 12
